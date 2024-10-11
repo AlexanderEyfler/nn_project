07a88daeb5b780df6f2b912c0e4f0c3f8a3abc25
Александр Эйфлер <eyfler.imp@gmail.com>
2024-10-11 19:59:09 +0500

    final fixes before deploy

--- pages/agricultural_crops.py
+++ pages/agricultural_crops.py
@@ -4,12 +4,17 @@ import torchvision.models as models
 from PIL import Image
 from torchvision import transforms
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else
+                      "mps" if torch.backends.mps.is_available() else
+                      "cpu")
+
 # Загрузите сохранённое состояние модели
 model_path = 'models/danila/model_50.pth'
 model = models.resnet50(pretrained=False)
 num_ftrs = model.fc.in_features
 model.fc = torch.nn.Linear(num_ftrs, 30)  # 30 - количество классов в задаче
-model.load_state_dict(torch.load(model_path))
+model.load_state_dict(torch.load(model_path, map_location=DEVICE))
+model.to(DEVICE)
 model.eval()
 
 # Функция для предсказания
@@ -19,11 +24,15 @@ def predict(image):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    image = transform(image).unsqueeze(0)
+    image = transform(image).to(DEVICE).unsqueeze(0)
     with torch.no_grad():
         outputs = model(image)
     _, predicted = torch.max(outputs, 1)
-    return predicted.item()
+    classes = ['tomato', 'pineapple', 'Tobacco-plant', 'jowar', 'Olive-tree', 'Fox_nut(Makhana)', 
+             'banana', 'sunflower', 'Cherry', 'Lemon', 'Cucumber', 'Coffee-plant', 'vigna-radiati(Mung)', 
+           'cardamom', 'cotton', 'maize', 'sugarcane', 'papaya', 'gram', 'mustard-oil', 'Pearl_millet(bajra)', 
+           'chilli', 'tea', 'jute', 'coconut', 'clove', 'wheat', 'soyabean', 'almond', 'rice']
+    return classes[predicted.item()]
 
 # Интерфейс Streamlit
 st.title('Классификация изображений с помощью ResNet50')
